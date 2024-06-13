531542d3de5f1efd542d2a4a3d6089e24d2dff4a
Bone Lira <bone-lira@hotmail.com>
2024-06-13 02:30:28 -0300

    add: produto

---
+++ src/Produto.java
@@ -0,0 +1,48 @@
+public class Produto {
+
+    private int id;
+    private String nome;
+    private int quantidade;
+    private String descricao;
+    private double valor;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+}
