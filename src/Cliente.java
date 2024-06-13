736bc32b1309e0431b31e360b0e64b08ec461ee8
Roosevelt Bispo <roosevelt.2019206592@unicap.br>
2024-06-13 01:56:39 -0300

    Classe cliente e sexo

---
+++ src/Cliente.java
@@ -0,0 +1,51 @@
+import java.util.Date;
+
+public class Cliente {
+
+    private int id;
+    private String nome;
+    private Sexo sexo;
+    private int idade;
+    private Date nascimento;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Sexo getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(Sexo sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public Date getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(Date nascimento) {
+        this.nascimento = nascimento;
+    }
+}
