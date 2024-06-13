3ac13822df5135f9df09e5eb481796407ab303c1
Bone Lira <bone-lira@hotmail.com>
2024-06-13 02:37:34 -0300

    alter: main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
 public class Main {
-    
+    public static void main(String[] args) {
+        ClienteService clienteService = new ClienteService();
+
+        clienteService.addCliente("Bone Lira", Sexo.M, 23, java.sql.Date.valueOf("2001-01-01"));
+    }
 }
